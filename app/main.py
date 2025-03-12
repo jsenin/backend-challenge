cbba841b982e0037cf96e9bb08608955854fde38
jorge <jorge@senin.org>
2025-03-13 00:32:58 +0100

    Refactor extract method

--- app/main.py
+++ app/main.py
@@ -4,14 +4,17 @@ from app.assistanace_request import AssistanceRequest
 from app.infrastructure import factory
 
 fastapi = FastAPI()
-
+class AssistanceRequestByMail:
+    @staticmethod
+    def send(message:str):
+        mail_client = factory.build_mail_client()
+        mail_client.send(mailto='pricing@fake.com', mail_from='bot@landbot.io', subject='Assistance request',
+                         message=message)
 
 @fastapi.post("/request_assistance")
 def post(assistance_request: AssistanceRequest):
     if assistance_request.topic == 'Pricing':
-        mail_client = factory.build_mail_client()
-        mail_client.send(mailto='pricing@fake.com', mail_from='bot@landbot.io', subject='Assistance request',
-                  message=assistance_request.description)
+        AssistanceRequestByMail.send(assistance_request.description)
         return {"message": "Requested!"}
 
     if assistance_request.topic == 'Sales':
