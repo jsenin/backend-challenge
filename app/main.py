7a2476e665db4943ccb2f5c25885e09755bb6f53
jorge <jorge@senin.org>
2025-03-13 01:12:04 +0100

    Use CommandHandler at endpoint

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ import dataclasses
 
 from fastapi import FastAPI
 
-from app.assistanace_request import AssistanceRequest
+from app.assistanace_request_command import AssistanceRequestCommand
 from app.infrastructure import factory
 
 fastapi = FastAPI()
@@ -13,10 +13,7 @@ class DeliveryStrategy:
     def send(self, message: str):
         pass
 
-@dataclasses.dataclass
-class AssistanceRequestCommand:
-    topic: str
-    description: str
+
 
 class AssistanceRequestCommandHandler:
     def __init__(self, delivery_strategies: dict[str, DeliveryStrategy]):
@@ -38,11 +35,8 @@ class SlackDeliveryStrategy(DeliveryStrategy):
         slack_client.send_message('sales', message=message)
 
 @fastapi.post("/request_assistance")
-def post(assistance_request: AssistanceRequest):
-    if assistance_request.topic == 'Pricing':
-        MailDeliveryStrategy.send(assistance_request.description)
-        return {"message": "Requested!"}
+def post(assistance_request: AssistanceRequestCommand):
+    command_handler = AssistanceRequestCommandHandler({'Pricing' : MailDeliveryStrategy(), 'Sales' : SlackDeliveryStrategy()})
+    command_handler.handle(assistance_request)
+    return {"message": "Requested!"}
 
-    if assistance_request.topic == 'Sales':
-        SlackDeliveryStrategy.send(assistance_request.description)
-        return {"message": "Requested!"}
