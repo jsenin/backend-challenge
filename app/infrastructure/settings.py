cb784c8915ec3064e2cbf2fc8d1e288a6e53a991
jorge <jorge@senin.org>
2025-03-13 11:18:09 +0100

    Use config settings at `config/config.ini`

--- app/infrastructure/settings.py
+++ app/infrastructure/settings.py
@@ -1,4 +1,7 @@
+import configparser
 import dataclasses
+import os
+
 
 @dataclasses.dataclass
 class MailSettings:
@@ -22,16 +25,32 @@ class MailDeliverySettings:
 class SlackDeliverySettings:
     channel:str
 
+
+config_file = os.path.dirname(__file__) + "/../../config/config.ini"
+config = configparser.ConfigParser()
+config.read(config_file)
+
+
 def mail_settings():
-    return MailSettings(smtp_server='mailpit', smtp_port=1025, username='test', password='password')
+    smtp_server = os.getenv("MAIL_HOST") or config["MAIL"]["host"]
+    smtp_port = os.getenv("MAIL_PORT") or config["MAIL"]["port"]
+    username = os.getenv("MAIL_USER") or config["MAIL"]["user"]
+    password = os.getenv("MAIL_PASSWORD") or config["MAIL"]["password"]
+
+    return MailSettings(smtp_server=smtp_server, smtp_port=int(smtp_port), username=username, password=password)
 
 def slack_settings():
-    return SlackSettings(token='faketoken')
+    api_token = os.getenv("SLACK_API_TOKEN") or config["SLACK"]["api_token"]
+    return SlackSettings(token=api_token)
 
 
 def mail_delivery_settings():
-    return MailDeliverySettings(mailto='pricing@landbot.io', mail_from='assistance@landbot.io', subject='Assistance request')
+    mailto = config["PRICING"]["mailto"]
+    mail_from = config["PRICING"]["mail_from"]
+    subject = config["PRICING"]["subject"]
+    return MailDeliverySettings(mailto=mailto, mail_from=mail_from, subject=subject)
 
 
 def slack_delivery_settings():
-    return SlackDeliverySettings(channel='sales')
+    slack_channel = config["SALES"]["slack_channel"]
+    return SlackDeliverySettings(channel=slack_channel)
